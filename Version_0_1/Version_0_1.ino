8139f0d21fd14fa57bdbe1d9fd29629edfa01c3b
L33t.uk <56431771+L33t-dot-UK@users.noreply.github.com>
2019-10-11 09:53:48 +0100

    Update Version_0_1.ino
    Code to give MIPR basic motor functions and Bluetooth control.

--- Version_0_1/Version_0_1.ino
+++ Version_0_1/Version_0_1.ino
@@ -1,1 +1,133 @@
-//
+/*
+// Author David Bradshaw
+// Basic funcitons for MIPR
+// visit https://www.l33t.uk/MIPR for more information
+*/
+
+int enRight = 9;
+int enLeft = 5;
+int i1A = 4; 
+int i2A = 3;
+int i3A = 8;
+int i4A = 7;
+
+int LEFTSPEED = 254;
+int RIGHTSPEED = 254;
+
+void setup() 
+{
+    Serial.begin(57600); //Use this baud rate if your using the
+							//HC-05 to program the Arduino
+	
+	//Serial.begin(9600); //Use this baud rate if you havent changed the
+							//Parameters on the HC-05 this is the default
+							//baud.
+}
+
+void loop() 
+{
+    char command = listenForBTCommands();
+    if (command != "")
+    {
+        executeBTcommand(command);
+    }
+}
+
+/*
+ * MOTOR FUNCTIONS
+ */
+
+ void Forwards(int leftSpeed, int rightSpeed)
+ {
+    digitalWrite(i1A,LOW);
+    digitalWrite(i2A,HIGH);
+    digitalWrite(i3A,HIGH);
+    digitalWrite(i4A,LOW);
+    analogWrite(enLeft, leftSpeed);
+    analogWrite(enRight, rightSpeed);
+ }
+  void Backwards(int leftSpeed, int rightSpeed)
+ {
+    digitalWrite(i1A,HIGH);
+    digitalWrite(i2A,LOW);
+    digitalWrite(i3A,LOW);
+    digitalWrite(i4A,HIGH);
+    analogWrite(enLeft, leftSpeed);
+    analogWrite(enRight, rightSpeed);
+ }
+ void Right(int leftRotationSpeed, int rightRotationSpeed)
+ {
+    digitalWrite(i1A,LOW);
+    digitalWrite(i2A,HIGH);
+    digitalWrite(i3A,LOW);
+    digitalWrite(i4A,HIGH);
+    analogWrite(enLeft, leftRotationSpeed);
+    analogWrite(enRight, rightRotationSpeed);
+ } 
+ void Left(int leftRotationSpeed, int rightRotationSpeed)
+ {
+    digitalWrite(i1A,HIGH);
+    digitalWrite(i2A,LOW);
+    digitalWrite(i3A,HIGH);
+    digitalWrite(i4A,LOW);
+    analogWrite(enLeft, leftRotationSpeed);
+    analogWrite(enRight, rightRotationSpeed);
+ }
+ void Halt()
+ {
+    digitalWrite(i1A,LOW);
+    digitalWrite(i2A,LOW);
+    digitalWrite(i3A,LOW);
+    digitalWrite(i4A,LOW);
+    analogWrite(enLeft, 254);
+    analogWrite(enRight, 254);
+ }
+  void softStop()
+ {
+    analogWrite(enLeft, 0);
+    analogWrite(enRight, 0);
+ }
+
+/*
+ * BLUETOOTH CONTROLS
+ */
+
+ char listenForBTCommands()
+ {
+    char BTcommand = "";
+    if (Serial.available())
+    {
+        BTcommand = Serial.read();
+    }
+    return BTcommand;
+ }
+
+ void executeBTcommand(char command)
+ {
+    if (command == 'F') //Forwards
+    {
+        softStop();
+        Forwards(LEFTSPEED, RIGHTSPEED);
+    }
+        if (command == 'B') //Backwards
+    {
+        softStop();
+        Backwards(LEFTSPEED, RIGHTSPEED);
+    }
+        if (command == 'L') //Left turn
+    {
+        softStop();
+        Left(LEFTSPEED, RIGHTSPEED);
+    }
+        if (command == 'R') //Right turn
+    {
+        softStop();
+        Right(LEFTSPEED, RIGHTSPEED);
+    }
+        if (command == 'S') //Stop
+    {
+        softStop();
+        delay(1);
+        Halt();
+    }
+ }
