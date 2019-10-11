cc60bac45b961b177aab3dc49e8d12ef6f0795f1
L33t.uk <56431771+L33t-dot-UK@users.noreply.github.com>
2019-10-11 10:14:02 +0100

    Create Motor_Functions.ino

---
+++ Battery_Board_Setup/Motor_Functions.ino
@@ -0,0 +1,52 @@
+//This tab contains all motor functions
+
+void Forwards(int leftSpeed, int rightSpeed)
+ {
+    digitalWrite(i1A,LOW);
+    digitalWrite(i2A,HIGH);
+    digitalWrite(i3A,HIGH);
+    digitalWrite(i4A,LOW);
+    analogWrite(enLeft, leftSpeed);
+    analogWrite(enRight, rightSpeed);
+ }
+  void Backwards(int leftSpeed, int rightSpeed)
+ {
+    digitalWrite(i1A,HIGH);
+    digitalWrite(i2A,LOW);
+    digitalWrite(i3A,LOW);
+    digitalWrite(i4A,HIGH);
+    analogWrite(enLeft, leftSpeed);
+    analogWrite(enRight, rightSpeed);
+ }
+ void Right(int leftRotationSpeed, int rightRotationSpeed)
+ {
+    digitalWrite(i1A,LOW);
+    digitalWrite(i2A,HIGH);
+    digitalWrite(i3A,LOW);
+    digitalWrite(i4A,HIGH);
+    analogWrite(enLeft, leftRotationSpeed);
+    analogWrite(enRight, rightRotationSpeed);
+ } 
+ void Left(int leftRotationSpeed, int rightRotationSpeed)
+ {
+    digitalWrite(i1A,HIGH);
+    digitalWrite(i2A,LOW);
+    digitalWrite(i3A,HIGH);
+    digitalWrite(i4A,LOW);
+    analogWrite(enLeft, leftRotationSpeed);
+    analogWrite(enRight, rightRotationSpeed);
+ }
+ void Halt()
+ {
+    digitalWrite(i1A,LOW);
+    digitalWrite(i2A,LOW);
+    digitalWrite(i3A,LOW);
+    digitalWrite(i4A,LOW);
+    analogWrite(enLeft, 254);
+    analogWrite(enRight, 254);
+ }
+  void softStop()
+ {
+    analogWrite(enLeft, 0);
+    analogWrite(enRight, 0);
+ }
