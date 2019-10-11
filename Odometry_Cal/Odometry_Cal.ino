87403c276276ef50ddbbf09d68915bbce5efb066
L33t.uk <56431771+L33t-dot-UK@users.noreply.github.com>
2019-10-11 10:11:07 +0100

    Update Odometry_Cal.ino
    Use this code to calibrate the Odometry module

--- Odometry_Cal/Odometry_Cal.ino
+++ Odometry_Cal/Odometry_Cal.ino
@@ -7,7 +7,126 @@
 
 int addr = 0; //EEPROM ADDRESS
 
+#include <EEPROM.h> //Include this library to access the EEPROM on the Arduino, 
 
+int enRight = 9;
+int enLeft = 5;
+int i1A = 4; 
+int i2A = 3;
+int i3A = 8;
+int i4A = 7;
+
+int leftMotorSensor = A7;
+int rightMotorSensor = A6;
+
+//These values will be set by 01_run_Wheel_Sensor_Cal() and saved in your EEPROM;
+int maxRight = 0;
+int minRight = 1024;
+int maxLeft = 0;
+int minLeft = 1024;
+
+int meanLeft = 500;
+int meanRight = 500;
+
+
+int startTime = 0;
+int loopTime = 0;
+
+int RESTART_MODE = 0;
+
+void setup() 
+{
+
+    Serial.begin(57600);
+
+    //Lets see the maximum and minimum values for the left and right
+    //Sensor. This will be different for each robot depending on the
+    //angle of the TCRT5000 and it's distance from the wheel
+
+    if (int(EEPROM.read(0)) == 128)
+    {
+        maxRight = int(EEPROM.read(1)) * 4;
+        minRight = int(EEPROM.read(2)) * 4;
+        maxLeft = int(EEPROM.read(3)) * 4;
+        minLeft = int(EEPROM.read(4)) * 4;
+        meanRight = int(EEPROM.read(6)) * 4;
+        meanLeft = int(EEPROM.read(7)) * 4;
+        Serial.println("Odometry threshold values already in EEPROM");
+        Serial.print("meanRight");
+        Serial.print(": ");
+        Serial.print(meanRight);
+        Serial.print("      ");
+        Serial.print("meanLeft");
+        Serial.print(": ");
+        Serial.print(meanLeft);
+        Serial.print("      ");
+        Serial.print("rightMax");
+        Serial.print(": ");
+        Serial.print(maxRight);
+        Serial.print("      ");
+        Serial.print("rightMin");
+        Serial.print(": ");
+        Serial.print(minRight);
+        Serial.print("      ");
+        Serial.print("leftMax");
+        Serial.print(": ");
+        Serial.print(maxLeft);
+        Serial.print("      ");
+        Serial.print("leftMin");
+        Serial.print(": ");
+        Serial.println(minLeft);
+    }
+    else
+    {
+        Serial.println("No values found; running calibration, this will take around 5 seconds");
+        _01_run_Wheel_Sensor_Cal(true);
+    }
+
+    RESTART_MODE = int(EEPROM.read(5));  //Dictates what mode to start MIPR in
+    Serial.println(RESTART_MODE);
+    if (RESTART_MODE == 48) //Default Mode 0
+    {
+         Serial.println("MIPR STARTED IN DEFAULT MODE");
+         //MIPR WILL WAIT FOR COMMANDS IN THIS MODE
+    }
+    else if (RESTART_MODE == 49) //MODE 1
+    {
+        //DISPLAYS PULSE TRAIN FOR LEFT AND RIGHT WHEEL TO CHECK THE SETUP AND THRESHOLDS
+        Serial.println("MIPR STARTED PULSE DISPLAY MODE");
+    }
+    else if (RESTART_MODE == 50) //MODE 2
+    {
+        Serial.println("MIPR STARTED IN MODE 2");
+        //NOT IMPLEMENTED
+    }
+
+    else
+    {
+         Serial.println("NO RESTART MODE SELECTED");
+    }
+}
+
+void loop() 
+{
+    //We will time how long it takes for the main loop to execute so we know if our odometry calculations are accurate
+    startTime = millis();
+
+    //INSERT CODE HERE
+    if (RESTART_MODE == 49)
+    {
+        //Pulse display mode used to test thresholds
+        Forwards(128,128);
+        _01_Display_Pulse_Train();
+    }
+    
+    char command = listenForBTCommands();
+    if (command != "")
+    {
+        executeBTcommand(command);
+    }
+    
+    loopTime = millis() - startTime; //This must be below 4mS for accurate 
+}
 
 void _01_run_Wheel_Sensor_Cal(boolean toPrint)
 {
