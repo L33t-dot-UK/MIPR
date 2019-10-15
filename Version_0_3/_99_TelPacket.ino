061313d371fc45910bbbbf48cd5b8e87ceb75440
L33t.uk <56431771+L33t-dot-UK@users.noreply.github.com>
2019-10-15 14:56:24 +0100

    Create _99_TelPacket.ino

---
+++ Version_0_3/_99_TelPacket.ino
@@ -0,0 +1,19 @@
+/*
+ * Copyright David Bradshaw 2019
+ * 
+ */
+
+void build_Tel_Packet()
+{
+    Tel_Packet = (String)leftLDRval_A  + ',' +  (String)rightLDRval_A + ',' +  (String)left_Velocity  + ',' +  (String)right_Velocity + ',' + (String)battVoltage + ',' + (String)loopTime;
+
+    if(OdoMod_Installed == true && battVoltage < 3.58)
+    {
+        Serial.print("Battery Voltage too Low ");
+        Serial.print(battVoltage);
+        Serial.println(" Volts");
+        Halt();
+        speaker_on();
+        while(1){} //infinate loop so the robot will not respond
+    }
+}
