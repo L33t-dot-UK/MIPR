bc061da5dec4c2a9da7d661aafc0e6eec336db58
L33t.uk <56431771+L33t-dot-UK@users.noreply.github.com>
2019-10-18 17:37:28 +0100

    Update _99_TelPacket.ino

--- Version_0_5/_99_TelPacket.ino
+++ Version_0_5/_99_TelPacket.ino
@@ -14,7 +14,7 @@ void build_Tel_Packet()
         {
             //stop the motors and rest for 10 seconds allowing the load to be taken off the battery
             digitalWrite(13, HIGH); //Turn on the Arduino's LED indicating that the robot is resting for 10 seconds
-            halt();
+            Halt();
             delay(10000);
             isFirst = false;
             digitalWrite(13, LOW);
